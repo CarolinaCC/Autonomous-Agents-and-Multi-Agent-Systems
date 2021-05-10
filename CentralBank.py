7347f7461834daf4ea8c149d88cd396fb3af90b5
ist187641 <carolina.carreira@tecnico.ulisboa.pt>
2021-05-10 14:01:22 +0100

    add first main things

---
+++ CentralBank.py
@@ -0,0 +1,21 @@
+
+
+import sys
+
+agents = []
+runs = 40
+
+
+def setup():
+
+
+
+
+
+# -----------------------------------------------RUNNING_CODE-----------------------------------------------------------
+
+# quando fazemos python.centralbank.py é true, mas e fizermos apenas import centralbank não é
+if __name__ == "__main__":
+    setup()
+
+
