f752ca10aa3ae592f401037d1baf1cd32517bb20
SebastiaoAlmeida <48730226+SebastiaoAlmeida@users.noreply.github.com>
2021-05-22 02:05:26 +0100

    Skeleton of some events

--- event/event.py
+++ event/event.py
@@ -17,3 +17,34 @@ class Event:
 class NoneEvent:
     def applies_to_stock(self):
         return False
+
+class ElonMuskPositiveTweetEvent(Event): #Uma companhia sobe bastante. Se calhar volta a descer rapidamente
+    def __init__(self, name, modifier):
+        super().__init__(name,modifier)
+        #self.stocks_affected = Só quero que afecte uma stock, se calhar um one-hot array?
+        #ou um nome de stock? Para isso preciso de ter lista dos nomes
+        
+        
+class ElonMuskNegativeTweetEvent(Event): #Uma companhia desce bastante. Se calhar volta a subir rapidamente
+    def __init__(self, name, modifier):
+        super().__init__(name,modifier)
+        #self.stocks_affected = Só quero que afecte uma stock, se calhar um one-hot array?
+        #ou um nome de stock? Para isso preciso de ter lista dos nomes   
+        
+class GlobalPandemicEvent(Event): #Quase todas as companhias descem. Umas poucas sobem (farmacêuticas)
+    def __init__(self, name, modifier):
+        super().__init__(name,modifier)
+        
+class TechBreakthroughEvent(Event): #Uma companhia sobe e companhias rivais descem
+    def __init__(self, name, modifier):
+        super().__init__(name,modifier)
+        
+class WarrenBuffetListEvent(Event): #Warren Buffet invests in a list of stocks and they go up
+    def __init__(self, name, modifier):
+        super().__init__(name,modifier)
+        
+class IlegalMonopolyFineEvent(Event): #2+ companies are fined for having illegal monopoly (ou entao concorrencia desleal) and their shares drop somewhat
+
+
+    
+    
