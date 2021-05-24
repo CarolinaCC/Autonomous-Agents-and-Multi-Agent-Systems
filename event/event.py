6cf15bc84a903b5128286f375a5d479389b995ea
SebastiaoAlmeida <48730226+SebastiaoAlmeida@users.noreply.github.com>
2021-05-24 17:43:23 +0100

    Preparing architecture for more complex events

--- event/event.py
+++ event/event.py
@@ -2,20 +2,19 @@
 
 
 class Event:
-    def __init__(self, name, modifier, num_steps, stocks=None):
+    def __init__(self, name, modifiers, num_steps, stocks=None):
         if stocks is None:
             stocks = []
         self.name = name
-        self.modifier = modifier
+        self.modifiers = modifiers
         self.num_steps = num_steps
         self.stocks = stocks
 
     # updates the value of the stock passed relating to the event.
-    def update_stock(self, stock):
-        stock.update_price(self.modifier * stock.price)
-
+    def update_stock(self, stock, index):
+        stock.update_price(self.modifiers[index] * stock.price)
     def update(self):
-        for stock in self.stocks:
+        for index,stock in enumerate(self.stocks):
             self.update_stock(stock)
         self.num_steps -= 1
 
@@ -27,38 +26,38 @@ class NoneEvent(Event):
 
 
 class ElonMuskPositiveTweetEvent(Event):  # Uma companhia sobe bastante. Se calhar volta a descer rapidamente
-    def __init__(self, name, modifier):
-        super().__init__(name, modifier)
+    def __init__(self, name, modifiers):
+        super().__init__(name, modifiers)
         # self.stocks_affected = Só quero que afecte uma stock, se calhar um one-hot array?
         # ou um nome de stock? Para isso preciso de ter lista dos nomes
 
 
 class ElonMuskNegativeTweetEvent(Event):  # Uma companhia desce bastante. Se calhar volta a subir rapidamente
-    def __init__(self, name, modifier):
-        super().__init__(name, modifier)
+    def __init__(self, name, modifiers):
+        super().__init__(name, modifiers)
         # self.stocks_affected = Só quero que afecte uma stock, se calhar um one-hot array?
         # ou um nome de stock? Para isso preciso de ter lista dos nomes
 
 
 class GlobalPandemicEvent(Event):  # Quase todas as companhias descem. Umas poucas sobem (farmacêuticas)
-    def __init__(self, name, modifier):
-        super().__init__(name, modifier)
+    def __init__(self, name, modifiers):
+        super().__init__(name, modifiers)
 
 
 class TechBreakthroughEvent(Event):  # Uma companhia sobe e companhias rivais descem
-    def __init__(self, name, modifier):
-        super().__init__(name, modifier)
+    def __init__(self, name, modifiers):
+        super().__init__(name, modifiers)
 
 
 class WarrenBuffetListEvent(Event):  # Warren Buffet invests in a list of stocks and they go up
-    def __init__(self, name, modifier):
-        super().__init__(name, modifier)
+    def __init__(self, name, modifiers):
+        super().__init__(name, modifiers)
 
 
 class IlegalMonopolyFineEvent(Event):  # 2+ companies are fined for having illegal monopoly (ou entao concorrencia
     # desleal) and their shares drop somewhat
-    def __init__(self, name, modifier):
-        super().__init__(name, modifier)
+    def __init__(self, name, modifiers):
+        super().__init__(name, modifiers)
 
 
 class EventIterator:
